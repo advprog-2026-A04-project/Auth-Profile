4eb22fc645569b1e26819c5a703a20abefb965aa
omarsuyuf <omarsuyufw@gmail.com>
2026-02-19 21:09:59 +0700

    feat: add User model

---
+++ src/main/java/id/ac/ui/cs/advprog/auth_profile/model/User.java
@@ -0,0 +1,29 @@
+package id.ac.ui.cs.advprog.auth_profile.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column
+    private String fullName;
+
+    @Column
+    private String role = "TITIPERS";
+}
